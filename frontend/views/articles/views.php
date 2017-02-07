eab524ec1acd5a006c2a068686c34c814f4e4809
Alexsey <alexsey_89@bk.ru>
2017-02-07 19:02:52 +0300

    fix right column

--- frontend/views/articles/views.php
+++ frontend/views/articles/views.php
@@ -24,8 +24,8 @@ $this->registerMetaTag(['name' => 'description','content' => 'Госзакупк
                     <?php foreach ($newNews as $news) { ?>
                         <div class="content-of-slide content-of-slide-all-news">
                             <?php echo \yii\helpers\Html::img(['/upload/'.$news->picture_text]);?>
-                            <h5><?php echo $news->title;?></h5>
-                            <p><?php echo \yii\helpers\StringHelper::truncateWords($news->annotation, 10, ''); ?></p>
+                            <p><a href="<?php echo \yii\helpers\Url::to(['articles/views', 'id' => $news->id]);?>"><?php echo $news->title;?></a></p>
+                            <h5><?php echo \yii\helpers\StringHelper::truncateWords($news->annotation, 10, ''); ?></h5>
                         </div>
                     <?php } ?>
                 </div>
@@ -44,11 +44,13 @@ $this->registerMetaTag(['name' => 'description','content' => 'Госзакупк
         <aside class="aside-right-all-news">
             <div class="aside-body-all-news">
                 <?php foreach ($newNews as $news) { ?>
+                <a href="<?php echo \yii\helpers\Url::to(['articles/views', 'id' => $news->id]);?>">
                     <div class="content-of-slide content-of-slide-all-news item-of-aside">
                         <?php echo \yii\helpers\Html::img(['/upload/'.$news->picture_text]);?>
                         <h5><?php echo $news->title;?></h5>
                         <p><?php echo \yii\helpers\StringHelper::truncateWords($news->annotation, 10, ''); ?></p>
                     </div>
+                </a>
                 <?php } ?>
             </div>
         </aside>
--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -39,11 +39,13 @@ $this->registerMetaTag(['name' => 'description','content' => 'Госзакупк
         <aside class="aside-right-all-news">
             <div class="aside-body-all-news">
                 <?php foreach ($newNews as $news) { ?>
+                <a href="<?php echo \yii\helpers\Url::to(['news/views', 'id' => $news->id]);?>">
                     <div class="content-of-slide content-of-slide-all-news item-of-aside">
                         <?php echo \yii\helpers\Html::img(['/upload/'.$news->picture_text]);?>
                         <h5><?php echo $news->title;?></h5>
                         <p><?php echo \yii\helpers\StringHelper::truncateWords($news->annotation, 10, ''); ?></p>
                     </div>
+                 </a>
                 <?php } ?>
             </div>
         </aside>
