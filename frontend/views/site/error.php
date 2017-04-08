ad7a6eac41be56647a2d6856a6d846db2dd41c90
Alexsey <alexsey_89@bk.ru>
2017-04-09 00:52:37 +0300

    fix 404

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -7,7 +7,7 @@
 
 use yii\helpers\Html;
 
-$this->title = $name;
+$this->title = 'Страница не найдена - Ошибка 404';
 ?>
 <div class="block-of-news-wrp">
     <div class="container">
@@ -16,13 +16,29 @@ $this->title = $name;
                 <div class="col-xs-12">
                     <div class="one-news-position">
                         <div class="alert alert-danger">
-                            <?= nl2br(Html::encode($message)) ?>
+                            Страница не найдена - Ошибка 404
                         </div>
                     </div>
                 </div>
 
                 <div class="col-xs-12 hidden-xs hidden-md">
-                    <div class="one-news-position">
+                    <div class="">
+                        <a href="<?php echo \yii\helpers\Url::to(['/']);?>">Перейти на главную</a>
+                        <br>
+                        <br>
+                        <a href="<?php echo \yii\helpers\Url::to(['news/index']);?>">Новости о закупках</a>
+                        <br>
+                        <br>
+                        <a href="<?php echo \yii\helpers\Url::to(['articles/law']);?>">Законы о закупках</a>
+                        <br>
+                        <br>
+                        <a href="<?php echo \yii\helpers\Url::to(['articles/analytics']);?>">Аналитика закупок</a>
+                        <br>
+                        <br>
+                        <a href="<?php echo \yii\helpers\Url::to(['articles/suppliers']);?>">Поставщику</a>
+                        <br>
+                        <br>
+                        <a href="<?php echo \yii\helpers\Url::to(['articles/customers']);?>">Заказчику</a>
 
                     </div>
                 </div>
