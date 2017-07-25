f43fb2ab46c9ddfc49ee26fd4779b98ade2e42dd
alex <alexsey_89@bk.ru>
2017-07-25 22:01:09 +0300

    airline and fixes

--- frontend/models/AirlineForm.php
+++ frontend/models/AirlineForm.php
@@ -34,12 +34,34 @@ class AirlineForm extends Model
 
     public function sendEmail($email)
     {
-        return Yii::$app->mailer->compose()
+        /*return Yii::$app->mailer->compose()
             ->setTo($email)
             ->setFrom([$this->email])
             ->setSubject('Вопрос на прямую линию')
             ->setTextBody('Вопрос c сайта TORG94.ru: '.$this->text)
-            ->send();
+            ->send();*/
+        // текст письма
+        $message = '
+            <html>
+            <head>
+              <title>Вопрос c сайта TORG94.ru</title>
+            </head>
+            <body>
+              <p>email: '.$this->email.'</p>
+              <p>Вопрос c сайта TORG94.ru:</p>
+              <p>'. $this->text. '</p>
+            </body>
+            </html>
+            ';
+
+// Для отправки HTML-письма должен быть установлен заголовок Content-type
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Дополнительные заголовки
+
+// Отправляем
+        mail($email, 'Вопрос на прямую линию', $message, $headers);
     }
 
 }
