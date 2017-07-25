ca3913e67d2817f6cf5559608a5efca8d65d9ce4
alex <alexsey_89@bk.ru>
2017-07-25 22:03:46 +0300

    fix and add configs

--- frontend/models/AirlineForm.php
+++ frontend/models/AirlineForm.php
@@ -56,7 +56,7 @@ class AirlineForm extends Model
 
 // Для отправки HTML-письма должен быть установлен заголовок Content-type
         $headers  = 'MIME-Version: 1.0' . "\r\n";
-        $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+        $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
 // Дополнительные заголовки
 
